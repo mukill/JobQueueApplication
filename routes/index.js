7e761f01a2e4257e835635dd0ba6360c0e019944
mukill <mukil.loganathan@gmail.com>
2017-04-12 00:31:36 -0700

    Intalled dependencies and created Jobqueue class

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/addJob', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/checkJob', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 
 module.exports = router;
